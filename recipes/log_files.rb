a5f6ef268aa3d08914b7b67bae19968c41faf7d9
Joey Aghion <joey@aghion.com>
2014-02-20 12:49:28 -0500

    Ensure that /etc/rsyslog.d/papertrail_log_files.conf has correct permissions

--- recipes/log_files.rb
+++ recipes/log_files.rb
@@ -2,6 +2,7 @@ include_recipe "opsworks_papertrail::rsyslog_service"
 
 template "/etc/rsyslog.d/papertrail_log_files.conf" do
   source "papertrail_log_files.conf.erb"
+  mode 0644
   variables :log_files => node[:papertrail][:log_files], :destination => node[:papertrail][:destination]
   notifies :restart, resources(:service => "rsyslog")
 end
