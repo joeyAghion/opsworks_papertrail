82678fbc50b07dcf8308a2f5bd09cf6ea9136258
Joey Aghion <joey@aghion.com>
2014-02-20 12:51:08 -0500

    Add version: 0.2

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,7 @@ maintainer       "Artsy"
 maintainer_email "it@artsymail.com"
 license          "MIT"
 description      "Configures rsyslog to report application logs to papertrail."
+version          "0.2"
 
 recipe 'opsworks_papertrail::log_files', 'Configure rsyslog to report application logs to papertrail.'
 recipe 'opsworks_papertrail::haproxy', "Configure rsyslog to report haproxy notice-level logs to papertrail."
